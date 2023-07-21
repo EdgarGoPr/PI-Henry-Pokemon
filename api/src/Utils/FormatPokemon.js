4c169ac699f9d00ad7419c7af7669d30d60fbde0
EdgarGoPr <egdeprada@hotmail.com>
2023-07-21 14:07:03 -0300

    actualizacion de formateo, getDetail funciona api y db

--- api/src/Utils/FormatPokemon.js
+++ api/src/Utils/FormatPokemon.js
@@ -1,17 +1,126 @@
+// function formatPokemonData(pokemon) {
+//   const { id, name, sprites, stats, height, weight, types } = pokemon;
+
+//   const formattedPokemon = {
+//     id,
+//     name,
+//     image: sprites.front_default || data.image,
+//     health: stats[0].base_stat,
+//     attack: stats[1].base_stat,
+//     defense: stats[2].base_stat,
+//     speed: stats[5] ? stats[5].base_stat : null,
+//     height,
+//     weight,
+//     type: types.map((type) => type.type.name),
+//   };
+
+//   return formattedPokemon;
+// }
+
+// module.exports = { formatPokemonData };
+
+//----------------------------------------------------------------
+
+// const formatPokemonData = (pokemon) => {
+//   const { id, name, image, health, attack, defense, speed, height, weight, types } = pokemon;
+
+//   const formattedPokemon = {
+//     id,
+//     name,
+//     image: image || null,
+//     health,
+//     attack,
+//     defense,
+//     speed,
+//     height,
+//     weight,
+//     type: types.map((type) => type.name),
+//   };
+
+//   return formattedPokemon;
+// };
+
+// module.exports = { formatPokemonData };
+
+//--------------------------------------------------------------------------
+
+// function formatPokemonData(pokemon) {
+//   const { id, name, sprites, stats, height, weight, types } = pokemon;
+
+//   const formattedPokemon = {
+//     id,
+//     name,
+//     image: sprites?.front_default || "default_image_url", // Replace "default_image_url" with a default image URL if available
+//     health: stats[0]?.base_stat || 0, // Replace 0 with a default health value if available
+//     attack: stats[1]?.base_stat || 0, // Replace 0 with a default attack value if available
+//     defense: stats[2]?.base_stat || 0, // Replace 0 with a default defense value if available
+//     speed: stats[5]?.base_stat || null,
+//     height,
+//     weight,
+//     type: types.map((type) => type?.type?.name || null),
+//   };
+
+//   return formattedPokemon;
+// }
+
+// module.exports = { formatPokemonData };
+
+//---------------------------------------------------------------------------
+
+// function formatPokemonData(pokemon) {
+//   const { id, name, sprites, stats, height, weight, types } = pokemon;
+
+//   const formattedPokemon = {
+//     id,
+//     name,
+//     image: sprites?.front_default || sprites?.other?.["official-artwork"]?.front_default || "",
+//     health: stats?.[0]?.base_stat || 0,
+//     attack: stats?.[1]?.base_stat || 0,
+//     defense: stats?.[2]?.base_stat || 0,
+//     speed: stats.find((stat) => stat.stat.name === "speed")?.base_stat || 0,
+//     height,
+//     weight,
+//     type: types.map((type) => type.type.name),
+//   };
+
+//   return formattedPokemon;
+// }
+
+// module.exports = { formatPokemonData };
+
+//---------------------------------------------------------------------------
+
 function formatPokemonData(pokemon) {
-  const { id, name, sprites, stats, height, weight, types } = pokemon;
+  if (!pokemon) {
+    return null; // Return null if the pokemon data is not provided
+  }
+
+  const {
+    id,
+    image,
+    name,
+    sprites,
+    stats,
+    height,
+    weight,
+    types,
+    attack,
+    defense,
+    life,
+    speed,
+  } = pokemon;
 
   const formattedPokemon = {
     id,
     name,
-    image: sprites.front_default,
-    health: stats[0].base_stat,
-    attack: stats[1].base_stat,
-    defense: stats[2].base_stat,
-    speed: stats[5] ? stats[5].base_stat : null,
+    image: sprites?.front_default || image || "default_image_url", // Replace "default_image_url" with a default image URL if available
+    health: stats?.[0]?.base_stat || life || 0, // Replace 0 with a default health value if available
+    attack: stats?.[1]?.base_stat || attack || 0, // Replace 0 with a default attack value if available
+    defense: stats?.[2]?.base_stat || defense || 0, // Replace 0 with a default defense value if available
+    speed: stats?.[5]?.base_stat || speed || null,
     height,
     weight,
-    type: types.map((type) => type.type.name),
+    type: types?.map((type) => type?.type?.name || null) || [], // Return an empty array if types is not provided
   };
 
   return formattedPokemon;
--- api/src/controllers/PokemonsController.js
+++ api/src/controllers/PokemonsController.js
@@ -3,62 +3,130 @@ const { formatPokemonData } = require("../Utils/FormatPokemon");
 const { Pokemon, Type } = require("../db");
 
 const getAllPokemons = async () => {
-  const pokemons = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=9");
+  const apiResponse = await axios.get(
+    "https://pokeapi.co/api/v2/pokemon?limit=9"
+  );
+  const apiPokemons = apiResponse.data.results;
+  const dbPokemons = await Pokemon.findAll();
+  const allPokemons = [...apiPokemons, ...dbPokemons];
+
+  return allPokemons;
   // let pokeFormateo = formatPokemonData(pokemons.data)
   // return pokeFormateo;
-  return pokemons.data.results
+  // return pokemons.data.results
 };
+
+// const getPokemonName = async (name) => {
+//   try {
+//     const apiResponse = await axios.get(
+//       `https://pokeapi.co/api/v2/pokemon?limit=9`
+//     );
+//     let existingPokemon = await Pokemon.findByPk(name);
+//     const pokemonList = apiResponse.data.results;
+//     // console.log(pokemonList, 'pokemonList');
+//     const filteredPokemon = pokemonList.find(
+//       (p) => p.name == name.toLowerCase()
+//     );
+//     if (!filteredPokemon) {
+//       if (!existingPokemon) {
+//         return { message: `No hay pokemones llamados ${name}` };
+//       }
+//     }
+//     const pokemonResponse = await axios.get(filteredPokemon.url);
+//     const pokemonData = pokemonResponse.data;
+//     const formatPokemon = formatPokemonData(pokemonData);
+//     return formatPokemon;
+//   } catch (error) {
+//     throw new Error({ error: error.message });
+//   }
+// };
 
 const getPokemonName = async (name) => {
   try {
     const apiResponse = await axios.get(
       `https://pokeapi.co/api/v2/pokemon?limit=9`
     );
-    let existingPokemon = await Pokemon.findByPk(name);
     const pokemonList = apiResponse.data.results;
-    // console.log(pokemonList, 'pokemonList');
     const filteredPokemon = pokemonList.find(
-      (p) => p.name == name.toLowerCase()
+      (p) => p.name === name.toLowerCase()
     );
+
     if (!filteredPokemon) {
+      const existingPokemon = await Pokemon.findOne({
+        where: { name: name.toLowerCase() },
+      });
+
       if (!existingPokemon) {
         return { message: `No hay pokemones llamados ${name}` };
       }
+
+      const formatPokemon = formatPokemonData(existingPokemon);
+      return formatPokemon;
     }
+
     const pokemonResponse = await axios.get(filteredPokemon.url);
     const pokemonData = pokemonResponse.data;
+
     const formatPokemon = formatPokemonData(pokemonData);
     return formatPokemon;
   } catch (error) {
-    throw new Error({ error: error.message });
+    throw new Error( error.message );
   }
 };
 
+// const getPokemonDetail = async (id) => {
+//   if (id.length <= 4) {
+//     // console.log(id, 'idvfabgdshrv')
+//     // const pokemonDetalle = await pokemonData.find(id => id == pokemonData.id);
+//     const pokemonDetalle = await axios.get(
+//       `https://pokeapi.co/api/v2/pokemon/${id}`
+//     );
+//     let pokemonDetail = pokemonDetalle.data;
+//     return formatPokemonData(pokemonDetail);
+//   } else {
+//     const dbPokemon = {
+//       id: pokemonData.id,
+//       name: pokemonData.name,
+//       image: pokemonData.image,
+//       health: pokemonData.health,
+//       attack: pokemonData.attack,
+//       defense: pokemonData.defense,
+//       speed: pokemonData.speed,
+//       height: pokemonData.height,
+//       weight: pokemonData.weight,
+//       type: pokemonData.types.map((type) => type.type.name),
+//     };
+//     return dbPokemon;
+//   }
+// };
+
 const getPokemonDetail = async (id) => {
   if (id.length <= 4) {
-    // console.log(id, 'idvfabgdshrv')
-    // const pokemonDetalle = await pokemonData.find(id => id == pokemonData.id);
-    const pokemonDetalle = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${id}`
-    );
-    let pokemonDetail = pokemonDetalle.data;
-    return formatPokemonData(pokemonDetail);
+    try {
+      const pokemonDetalle = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      const pokemonDetail = pokemonDetalle.data;
+      return formatPokemonData(pokemonDetail);
+    } catch (error) {
+      throw new Error(`Error fetching Pokemon details from API: ${error.message}`);
+    }
   } else {
-    const dbPokemon = {
-      id: pokemonData.id,
-      name: pokemonData.name,
-      image: pokemonData.image,
-      health: pokemonData.health,
-      attack: pokemonData.attack,
-      defense: pokemonData.defense,
-      speed: pokemonData.speed,
-      height: pokemonData.height,
-      weight: pokemonData.weight,
-      type: pokemonData.types.map((type) => type.type.name),
-    };
-    return dbPokemon;
+    try {
+      const dbPokemon = await Pokemon.findByPk(id, {
+        include: Type,
+      });
+      if (!dbPokemon) {
+        throw new Error(`Pokemon with ID ${id} not found in the database.`);
+      }
+      const formattedPokemon = formatPokemonData(dbPokemon);
+      return formattedPokemon;
+    } catch (error) {
+      throw new Error(`Error fetching Pokemon details from database: ${error.message}`);
+    }
   }
 };
+
 
 // const createPokemon = async (pokemonData) => {
 //   try {
@@ -192,6 +260,41 @@ const getPokemonDetail = async (id) => {
 //   }
 // };
 
+// const createPokemon = async ({
+//   name,
+//   image,
+//   life,
+//   attack,
+//   defense,
+//   speed,
+//   height,
+//   weight,
+//   type,
+// }) => {
+//   let objPokemon = {
+//     name,
+//     image,
+//     life,
+//     attack,
+//     defense,
+//     speed,
+//     height,
+//     weight,
+//     types: type,
+//   };
+
+//   let allTypes = [];
+//   for (const typeName of type) {
+//     const newType = await Type.findOne({ where: { name: typeName } });
+//     allTypes.push(newType);
+//     // console.log('first', first);
+//   }
+
+//   const newPokemon = await Pokemon.create(objPokemon);
+//   await newPokemon.addTypes(allTypes);
+//   return newPokemon;
+// };
+
 const createPokemon = async ({
   name,
   image,
@@ -201,30 +304,40 @@ const createPokemon = async ({
   speed,
   height,
   weight,
-  // type, 
+  type,
 }) => {
-  let objPokemon = {
-    name,
-    image,
-    life, 
-    attack,
-    defense,
-    speed,
-    height,
-    weight,
-    // types: type, 
-  };
+  try {
+    // Verificar si el Pokémon ya existe en la base de datos
+    const existingPokemon = await Pokemon.findOne({ where: { name } });
 
-  // let allTypes = [];
-  // for (const typeName of type) { 
-  //   const newType = await Type.findOne({ where: { name: typeName } });
-  //   allTypes.push(newType);
-    // console.log('first', first);
-  // }
+    if (!existingPokemon) {
+      // Crear el nuevo Pokémon en la base de datos
+      const newPokemon = await Pokemon.create({
+        name,
+        image,
+        life,
+        attack,
+        defense,
+        speed,
+        height,
+        weight,
+      });
 
-  const newPokemon = await Pokemon.create(objPokemon);
-  // await newPokemon.addTypes(allTypes);
-  return newPokemon;
+      // Asociar los tipos existentes con el Pokémon utilizando sus nombres
+      for (const typeName of type) {
+        const types = await Type.findOne({ where: { name: typeName } });
+        if (type) {
+          await newPokemon.addType(types);
+        }
+      }
+      console.log('types', newPokemon.type)
+      return newPokemon;
+    } else {
+      throw new Error("Pokemon already exists");
+    }
+  } catch (error) {
+    throw new Error(`Error creating a new Pokemon: ${error.message}`);
+  }
 };
 
 
